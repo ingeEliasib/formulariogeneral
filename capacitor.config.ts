4b9cc8d392935704de764d424f818642d13d6372
ingeEliasib <ingenieroeliasibcadena@gmail.com>
2025-08-16 08:56:02 -0500

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.formulariogeneral',
+  appName: 'formulariogeneral',
+  webDir: 'www'
+};
+
+export default config;
